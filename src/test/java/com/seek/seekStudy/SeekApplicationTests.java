f016fe14d94062b1cc9c9c118e7392587d871cc9
cathy970816@gmail.com <cathy970816@gmail.com>
2023-05-15 22:10:44 +0900

    프로젝트 첫 커밋

---
+++ src/test/java/com/seek/seekStudy/SeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seek.seekStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
